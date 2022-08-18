48ce12a59892b6aabc7cb7cce2004c23d01e33c8
reda nabil <redanabil14@gmail.com>
2022-08-18 02:56:58 +0100

    Working on first part execution

--- pipex.c
+++ pipex.c
@@ -12,6 +12,26 @@
 
 #include "includes/pipex.h"
 
+static void	first_part(char *cmd, char *option, char **envp)
+{
+	char	*path;
+	char	*options;
+
+	options = (char **)malloc(sizeof(char *) * 3);
+	options[0] = ft_strdup(cmd);
+	options[1] = ft_stdup(option);
+	options[3] = NULL;
+	//Do I really need a third option?
+	path = get_command_path(cmd);
+	execve(path, options, envp);
+	//how can I free options before executing execve? 
+}
+
+static void	second_part()
+{
+
+}
+
 /*Main code, executes major instructions*/
 int	main(int argc, char *argv[], char **envp)
 {
@@ -19,21 +39,25 @@ int	main(int argc, char *argv[], char **envp)
 	char	*path;
 	pid_t	pid;
 
-	ft_printf("Hello\n");
-	pid = fork();
-	//this part below is executed by both the child and the parent
-
-	if (pid == -1)
-		error("Error forking!");
-
-	if (pid == 0)
+	if (argc == 5)
 	{
-		execve("/bin/ls", options, envp);
+		pid = fork();
+		if (pid == -1)
+			error("Forking error occured!");
+		if (pid == 0)
+		{
+			/*Here we execute the first part and store the output in the pipe*/
+			first_part(argv[1], argv[2], envp);
+		}
+		else
+		{
+			/*This is the parent's part, it needs to wait for the child to finish and then execute the second part*/
+			/*I need to wait for the child processes before executing the second part*/
+			ft_printf("This is the parent whose pid : %d talking!\n", (int)pid);
+			usleep(10000000);
+		}
 	}
 	else
-	{
-		ft_printf("This is the parent whose pid : %d talking!\n", (int)pid);
-		usleep(10000000);
-	}
+		error("Invalid arguments!");
 	return (0);
 }
